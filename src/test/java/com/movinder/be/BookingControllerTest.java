f9fb289cac25bf167a0f4f0b47f2c5bc6e268e9d
Hezz <heinrich@connect.hku.hk>
2022-12-20 22:43:41 +0800

    fix: move create booking test to BookingControllerTest.java

--- src/test/java/com/movinder/be/BookingControllerTest.java
+++ src/test/java/com/movinder/be/BookingControllerTest.java
@@ -1,11 +1,12 @@
 package com.movinder.be;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.movinder.be.entity.Food;
-import com.movinder.be.entity.Seat;
-import com.movinder.be.entity.Ticket;
-import com.movinder.be.repository.FoodRepository;
-import com.movinder.be.repository.TicketRepository;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.movinder.be.controller.dto.BookingRequest;
+import com.movinder.be.controller.dto.RequestItem;
+import com.movinder.be.entity.*;
+import com.movinder.be.repository.*;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.*;
 
 
 @SpringBootTest
@@ -29,10 +36,28 @@ public class BookingControllerTest {
     FoodRepository foodRepository;
     @Autowired
     TicketRepository ticketRepository;
+    @Autowired
+    MovieSessionRepository movieSessionRepository;
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    MovieRepository movieRepository;
+    @Autowired
+    BookingRepository bookingRepository;
+    @Autowired
+    CinemaRepository cinemaRepository;
+
 
     @BeforeEach
     public void clearDB() {
         foodRepository.deleteAll();
+        ticketRepository.deleteAll();
+        foodRepository.deleteAll();
+        movieSessionRepository.deleteAll();;
+        customerRepository.deleteAll();
+        movieRepository.deleteAll();
+        bookingRepository.deleteAll();
+        cinemaRepository.deleteAll();
     }
 
     @Test
@@ -123,6 +148,117 @@ public class BookingControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.seat.column").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(10));
     }
+
+    @Test
+    public void should_create_booking_when_perform_booking_given_booking_request_obj() throws Exception {
+        //given
+        /*
+        Movie
+         */
+        Movie movie = new Movie();
+        movie.setMovieName("Avengers");
+        movie.setDescription("Action movie");
+        movie.setDuration(100);
+        movie.setThumbnailUrl("http://testurl");
+        movie.setMovieSessionIds(new ArrayList<>());
+        movie.setLastShowDateTime(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.ofHours(0)));
+
+        Movie savedMovie = movieRepository.save(movie);
+
+        /*
+        Customer
+         */
+        Customer customer = new Customer();
+        customer.setCustomerName("name");
+        customer.setPassword("pass");
+        customer.setGender("Male");
+        customer.setStatus("available");
+        customer.setSelfIntro("intro");
+        customer.setAge(20);
+        customer.setShowName(false);
+        customer.setShowGender(true);
+        customer.setShowAge(true);
+        customer.setShowStatus(true);
+        Customer savedCustomer = customerRepository.save(customer);
+
+        /*
+        Cinema
+         */
+        Cinema cinema = new Cinema();
+        cinema.setAddress("address");
+        cinema.setCinemaName("MCL");
+        cinema.setFloorPlan(new ArrayList<>());
+        Cinema savedCinema = cinemaRepository.save(cinema);
+
+
+        /*
+        Movie Session
+         */
+        Pricing pricing = new Pricing("adult", 10);
+        MovieSession movieSession = new MovieSession();
+        String start = "1970-01-01T00:00:00";
+        LocalDateTime now = LocalDateTime.parse(start);
+        movieSession.setDatetime(now);
+        movieSession.setCinemaId(savedCinema.getCinemaId());
+        movieSession.setMovieId(savedMovie.getMovieId());
+        movieSession.setPricing(new ArrayList<Pricing>(){
+            {add(pricing);}
+        });
+
+        ArrayList<ArrayList<Boolean>> seatings = new ArrayList<ArrayList<Boolean>>() {
+            {
+                add(new ArrayList<Boolean>() {
+                    {
+                        add(false);
+                        add(true);
+                    }
+                });
+            }
+        };
+        movieSession.setAvailableSeatings(seatings);
+        MovieSession savedMoviesSession = movieSessionRepository.save(movieSession);
+
+
+        /*
+        Food
+         */
+
+        Food food = new Food();
+        food.setFoodName("coke");
+        food.setDescription("1L");
+        food.setPrice(10);
+        Food savedFood = foodRepository.save(food);
+
+        /*
+        Booking request
+         */
+
+        BookingRequest bookingRequest = new BookingRequest();
+        bookingRequest.setCustomerId(savedCustomer.getCustomerId());
+        bookingRequest.setMovieSessionId(savedMoviesSession.getSessionId());
+        bookingRequest.setTicketRequestItems(new ArrayList<>(Collections.singletonList(new RequestItem("adult", 1))));
+        bookingRequest.setFoodRequestItems(new ArrayList<>(Collections.singletonList(new RequestItem(savedFood.getFoodId(), 1))));
+        bookingRequest.setSeatingRequests(new ArrayList<>(Collections.singletonList(new Seat(0,1))));
+
+
+
+        String bookingRequestJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(bookingRequest);
+
+
+
+        //when & then
+        client.perform(MockMvcRequestBuilders.post("/booking/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(bookingRequestJson))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.bookingId").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.customerId").value(savedCustomer.getCustomerId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.movieSessionId").value(savedMoviesSession.getSessionId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.ticketIds", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.foodIds", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.bookingTime", instanceOf(String.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total").value(20));
+    }
+
 
 }
 
