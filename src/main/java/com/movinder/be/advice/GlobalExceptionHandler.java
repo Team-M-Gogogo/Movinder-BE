c5482951bf5b4ed01a39c04a52f5aaf954c42bdb
Hezz <heinrich@connect.hku.hk>
2022-12-20 13:47:33 +0800

    feat: implemented CORS with error response using exceptional handling

---
+++ src/main/java/com/movinder/be/advice/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.movinder.be.advice;
+
+import com.movinder.be.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({IdNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse idNotFound(Exception exception) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler({InvalidIDException.class,
+            MalformedRequestException.class,
+            ProvidedKeyAlreadyExistException.class,
+            RequestDataNotCompleteException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse malformedRequest(Exception exception) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+
+}
