77e064c77a365e58f2332d3ad7b5e7bd684728df
WONGCH9 <wongch9@oocl.com>
2022-12-20 14:46:15 +0800

    fix: fix CORSConfiguration

--- src/main/java/com/movinder/be/config/CORSConfiguration.java
+++ src/main/java/com/movinder/be/config/CORSConfiguration.java
@@ -14,7 +14,7 @@ public class CORSConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("http://localhost:3000");
+                        .allowedOrigins("*");
             }
         };
     }
